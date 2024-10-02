41ccce85071fd7cfeb0ed1c1c60ab79d94ec3282
SanFre10 <fregasantiago@gmail.com>
2024-10-02 12:46:26 -0300

    main route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,5 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from "next/server";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        res.status(200).json({  });
-    } else {
-        res.setHeader('Allow', ['GET']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
+export async function GET(req: NextRequest) {
+    return NextResponse.json({}, { status: 200 });
 }
