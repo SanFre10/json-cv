b1c5ba3db6903ce4f9fe382b10bc2bb48c65a023
SanFre10 <fregasantiago@gmail.com>
2024-10-14 11:38:16 -0300

    mongo db global client on prod

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -8,28 +8,20 @@ export const connectToDatabase = async () => {
   if (db) return db;
 
   if (!client) {
-    if (process.env.NODE_ENV === "development") {
-      // In development mode, use a global variable so that the value
-      // is preserved across module reloads caused by HMR (Hot Module Replacement).
-      let globalWithMongo = global as typeof globalThis & {
-        _mongoClient?: MongoClient;
-      };
+    let globalWithMongo = global as typeof globalThis & {
+      _mongoClient?: MongoClient;
+    };
 
-      if (!globalWithMongo._mongoClient) {
-        globalWithMongo._mongoClient = new MongoClient(uri);
-        console.log("New client dev");
-      }
-      client = globalWithMongo._mongoClient;
-    } else {
-      // In production mode, it's best to not use a global variable.
-      client = new MongoClient(uri);
+    if (!globalWithMongo._mongoClient) {
+      globalWithMongo._mongoClient = new MongoClient(uri);
       console.log("New client");
     }
+    client = globalWithMongo._mongoClient;
+
     await client.connect();
     console.log("Connected");
   }
 
   db = client.db(process.env.MONGODB_DB);
-
   return db;
 };
